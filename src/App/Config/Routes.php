2a13c138ec6a26f89e2009dcf3ff4a3d6e85b2b1
sacheex <sachithdhanushka00@gmail.com>
2024-09-25 01:06:15 +0530

    update views

--- src/App/Config/Routes.php
+++ src/App/Config/Routes.php
@@ -16,6 +16,8 @@ function registerRoutes(App $app)
     $app->get('/', [HomeController::class, 'home'], [AuthRequiredMiddleware::class]);
     $app->get('/about', [AboutController::class, 'about']);
     $app->get('/register', [AuthController::class, 'registerView'], [GuestOnlyMiddleware::class]);
+    $app->get('/register/role', [AuthController::class, 'registerRoleView'], [GuestOnlyMiddleware::class]);
+
     $app->post('/register', [AuthController::class, 'register']);
     $app->get('/login', [AuthController::class, 'loginView'], [GuestOnlyMiddleware::class]);
     $app->post('/login', [AuthController::class, 'login']);
