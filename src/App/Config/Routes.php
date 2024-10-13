3a3cfaf64e631cdd193130fc46f75a6219a44c4c
sacheex <sachithdhanushka00@gmail.com>
2024-10-14 03:12:00 +0530

    add course route

--- src/App/Config/Routes.php
+++ src/App/Config/Routes.php
@@ -6,7 +6,7 @@ declare(strict_types=1);
 
 namespace App\Config;
 
-use App\Controllers\{HomeController, AboutController, AuthController, ProfileController};
+use App\Controllers\{HomeController, AboutController, AuthController, ProfileController, Course, TeacherController};
 use App\Middleware\AuthRequiredMiddleware;
 use App\Middleware\GuestOnlyMiddleware;
 use Framework\App;
@@ -23,6 +23,8 @@ function registerRoutes(App $app)
     $app->post('/login', [AuthController::class, 'login']);
     $app->get('/logout', [AuthController::class, 'logout']);
     $app->get('/profile', [ProfileController::class, 'profile']);
+    $app->get('/teacher', [TeacherController::class, 'teacher']);
+    $app->get('/courses', [Course::class, 'course']);
     // This is how paths with router params are added
     $app->get('/auth/{param1}/something/{param2}', [AuthController::class, 'testParamRoute']);
 }
