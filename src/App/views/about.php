d6a95ad55268e69d93208e16149025c5aea1ee2f
dinuka2001 <dinukasahan2001@gmail.com>
2024-11-12 13:32:31 -0500

    add header of about page

--- src/App/views/about.php
+++ src/App/views/about.php
@@ -1,16 +1,32 @@
+<head>
+    <link rel="stylesheet" href="/assets/styles/about.css">
+</head>
+
 <?php include $this->resolve("partials/_header.php"); ?>
-
 <!-- Start Main Content Area -->
-<section
-    class="container mx-auto mt-12 p-4 bg-white shadow-md border border-gray-200 rounded">
-    <!-- Page Title -->
-    
-    <h3>About Page</h3>
+<section class="about">
+    <div class="row">
 
-    <hr />
+        <div class="image">
+            <img src="/assets/images/about-img.svg" alt="About Us" class="about-image">
+        </div>
 
-    <!-- Escaping Data -->
-    <p>Escaping Data: </p>
+        <div class="content">
+            <img src="/assets/images/left-quote-svgrepo-com.svg" alt="quote-svg" class="quote-svg">
+            <h3>Beyond the ordinary <br>
+                A new Era of learining await.
+            </h3>
+            <p>
+                Transform your learning experience through engaging, immersive education.
+            </p>
+            <div class="cta-buttons">
+                <a href="#" class="cta-primary">Get Started</a>
+                <a href="/courses" class="cta-secondary">Explore Courses</a>
+            </div>
+        </div>
+
+    </div>
+
 </section>
 <!-- End Main Content Area -->
 
