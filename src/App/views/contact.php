c91faa59cea47ae4ff28de5fde041ac34112b394
dinuka2001 <dinukasahan2001@gmail.com>
2024-11-22 19:38:02 +0530

    create contact page

--- src/App/views/contact.php
+++ src/App/views/contact.php
@@ -3,7 +3,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
 
     <!-- custom css file link  -->
-    <link rel="stylesheet" href="/assets/styles/contacts.css">
+    <link rel="stylesheet" href="/assets/styles/contact.css">
 
 </head>
 
@@ -15,11 +15,15 @@
 
         <div class="row">
 
-            <div class="image">
+            <div class="left-side">
+                <div class="hedeline">
+                    <h3>Reach Out</h3>
+                    <h2>Anytime!</h2>
+                </div>
+                <span>Have a question or idea? We’d love to hear from you—let’s make your experience better!</span>
+
                 <img src="/assets/images/contact-img.svg" alt="">
 
-                <h3>Reach Out Anytime!</h3>
-                <span>Have a question or idea? We’d love to hear from you—let’s make your experience better!</span>
             </div>
 
             <form action="" method="post">
@@ -27,28 +31,12 @@
                 <input id="name" type="text" placeholder="enter your name" name="name" required maxlength="50" class="input-feild inputs">
                 <input id="email" type="email" placeholder="enter your email" name="email" required maxlength="50" class="input-feild inputs">
                 <textarea id="message" name="msg" class="input-message inputs" placeholder="enter your message" required maxlength="1000" cols="30" rows="10"></textarea>
-
                 <input id="sendButton" type="submit" value="send message" class="btn" name="submit">
             </form>
 
         </div>
-
-        <div class="box-container">
-
-            <div class="box">
-                <i class="fas fa-phone"></i>
-                <h3>phone number</h3>
-                <a href="tel:1234567890">123-456-7890</a>
-                <a href="tel:1112223333">111-222-3333</a>
-            </div>
-
-            <div class="box">
-                <i class="fas fa-envelope"></i>
-                <h3>email address</h3>
-                <a href="#">leanhub@gmail.come</a>
-            </div>
-
-        </div>
+        <!-- <i class="fas fa-envelope"></i>
+        <i class="fas fa-phone"></i> -->
 
     </section>
 
