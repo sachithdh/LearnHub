6275e1b61e1b7acf417c75e226e799cbe16b778c
dinuka2001 <dinukasahan2001@gmail.com>
2024-11-23 14:45:35 +0530

    404 page done

--- src/App/Controllers/PageController.php
+++ src/App/Controllers/PageController.php
@@ -65,11 +65,8 @@ class PageController
     }
     public function notFound()
     {
-        // echo $this->view->render('notFound.php',[
-        //     'title' => "not-Found-404"
-        // ]);
-
-        http_response_code(404);
-        echo '404 Not Found';
+        echo $this->view->render('notFound.php', [
+            'title' => "not-Found-404"
+        ]);
     }
 }
