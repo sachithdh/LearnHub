149fd75a460eb990259a1f625680c42e5c4ce4a1
dinuka2001 <dinukasahan2001@gmail.com>
2024-11-28 00:20:56 +0530

    add page for help and support review

--- src/App/views/User/Admin/help_And_Support_Review.php
+++ src/App/views/User/Admin/help_And_Support_Review.php
@@ -1,4 +1,13 @@
+
 <?php include $this->resolve("partials/_header.php") ?>
 
+<head>
+    <link rel="stylesheet" href="assets/styles/User/Admin/help_and_support_management.css">
+</head>
+<body>
+    <section>
+        
+    </section>
+</body>
 
 <?php include $this->resolve("partials/_footer.php") ?>
